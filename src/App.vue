bc29aaf713eaf3e8dbb42256f931884d62f99b2d
zhouhang <1728451471@qq.com>
2018-11-05 22:16:59 +0800

    vue使用elementui的按钮，单独只引button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,24 @@
     <h1>{{ msg }}</h1>
     <img src="./img/logo.png">
     <input type="text" v-model="msg">
+
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button disabled>默认按钮</el-button>
+      <el-button type="primary" disabled>主要按钮</el-button>
+      <el-button type="success" disabled>成功按钮</el-button>
+      <el-button type="info" disabled>信息按钮</el-button>
+      <el-button type="warning" disabled>警告按钮</el-button>
+      <el-button type="danger" disabled>危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
